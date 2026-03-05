d5422dbdf1c19fed6ef6905cc4bf53806f4e9234
kanonren <kanonren0707@icloud.com>
2026-03-05 18:37:04 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,36 @@
-input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
-    radio.sendString("hallo")
-})
-input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
-    radio.sendString("test")
-})
 radio.onReceivedString(function (receivedString) {
-    radio.sendString(receivedString)
+    if (receivedString == "BEAT") {
+        let spielt_gerade = false
+        if (spielt_gerade == true) {
+            melodie = [
+            "C",
+            "C",
+            "G",
+            "G",
+            "A",
+            "H",
+            "C",
+            "H",
+            "A",
+            "A",
+            "F",
+            "F",
+            "E",
+            "E",
+            "D",
+            "D",
+            "C"
+            ]
+            taktposition += 1
+        } else if (taktposition >= melodie.length) {
+            taktposition = 0
+        }
+    }
 })
+let melodie: string[] = []
+let taktposition = 0
 radio.setGroup(42)
 basic.pause(randint(100, 500))
 let meinInstrument = randint(0, 2)
 basic.showNumber(meinInstrument)
 radio.sendString("Claim:" + meinInstrument)
-basic.forever(function () {
-	
-})
